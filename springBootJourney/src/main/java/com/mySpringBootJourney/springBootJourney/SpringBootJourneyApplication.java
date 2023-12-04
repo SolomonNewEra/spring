2e82c1a14f03ff2cae41ee2a0aee934b13c28c61
SolomonNewEra <150368257+SolomonNewEra@users.noreply.github.com>
2023-12-04 10:34:17 +0100

    updating files

---
+++ springBootJourney/src/main/java/com/mySpringBootJourney/springBootJourney/SpringBootJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.mySpringBootJourney.springBootJourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJourneyApplication.class, args);
+	}
+
+}
---
+++ springBootJourney/src/test/java/com/mySpringBootJourney/springBootJourney/SpringBootJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mySpringBootJourney.springBootJourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
